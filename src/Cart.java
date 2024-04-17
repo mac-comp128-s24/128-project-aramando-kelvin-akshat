692d8c9743e39a3dbbcbeac1eb1797bd83787c13
ksuelflo <134102709+ksuelflo@users.noreply.github.com>
2024-04-17 10:41:33 -0500

    Added Cart and Product

--- src/Cart.java
+++ src/Cart.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class Cart {
-    
+    private List<Product> items;
+
+    public Cart(){
+        this.items = new ArrayList<Product>();
+    }
+
+    public void addToCart(Product item){
+        items.add(item);
+    }
+
+    public void removeFromCart(Product item){
+        if (!items.remove(item)){
+            System.out.println("Item not in cart.");
+        }
+    }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,25 @@
 public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price){
+        name = this.name;
+        price = this.price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
