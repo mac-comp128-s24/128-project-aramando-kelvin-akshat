9ab32699e081e892bb1659e793de83f108cde739
ksuelflo <134102709+ksuelflo@users.noreply.github.com>
2024-04-24 09:55:47 -0500

    Added image to product

--- src/Product.java
+++ src/Product.java
@@ -1,7 +1,12 @@
+import edu.macalester.graphics.Image;
+
 public class Product {
     
     private String name;
     private Double price;
+    //Eventually each product will have an image associated with it, which we will display in UI.
+    private Image image;
+    
 
     public Product(String name, Double price){
         this.name = name;
